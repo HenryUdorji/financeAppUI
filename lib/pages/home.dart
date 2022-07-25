8e13e18c91a94561a9f075493d9dc790659ba7d4
ifechukwu <ifechukwu.udorji@accelerexholdings.com>
2022-07-25 17:18:37 +0100

    work in progress

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -16,19 +16,83 @@ class _HomePageState extends State<HomePage> {
       body: SafeArea(
         child: Column(
           children: [
-            Container(
-              height: 200,
-              decoration: BoxDecoration(
-                  color: color.Colors.accentColor,
-                  borderRadius: const BorderRadius.only(
-                      bottomLeft: Radius.circular(40),
-                      bottomRight: Radius.circular(40)
-                  )
-              ),
-            )
+            TopSection()
           ],
         ),
       ),
     );
   }
 }
+
+class TopSection extends StatelessWidget {
+  const TopSection({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          color: color.Colors.backgroundColor,
+          padding: const EdgeInsets.all(10),
+          alignment: Alignment.topCenter,
+          height: 300,
+        ),
+        Container(
+          padding: const EdgeInsets.all(10),
+          alignment: Alignment.topCenter,
+          height: 180,
+          decoration: BoxDecoration(
+              color: color.Colors.accentColor,
+              borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(40),
+                  bottomRight: Radius.circular(40)
+              )
+          ),
+        ),
+        Positioned(child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Expanded(child: Image.asset('assets/images/man.png', width: 40, height: 40, alignment: Alignment.topLeft,)),
+              Image.asset('assets/images/search.png', width: 25, height: 25, color: Colors.white,),
+              const SizedBox(width: 20,),
+              Image.asset('assets/images/bell.png', width: 25, height: 25, color: Colors.white,)
+            ],
+          ),
+        ),),
+        Positioned(
+          top: 80,
+          child: Container(
+            height: 150,
+            margin: const EdgeInsets.symmetric(horizontal: 30),
+            padding: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20)
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Available Balance'),
+                        Text('18645')
+                      ],
+                    ),
+                    Image.asset('assets/images/man.png', width: 40, height: 40,),
+                  ],
+                )
+              ],
+            )
+          ),
+        ),
+      ],
+    );
+  }
+}
+

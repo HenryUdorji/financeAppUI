2d737059f1949aaff72eb3068658a5f45a745e85
ifechukwu <ifechukwu.udorji@accelerexholdings.com>
2022-07-26 05:10:57 +0100

    completed bottom navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:finance_app_ui/pages/home.dart';
+import 'package:finance_app_ui/home.dart';
 import 'package:flutter/material.dart';
 
 import 'package:finance_app_ui/colors.dart' as color;
